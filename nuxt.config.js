05dc7d3629db1742992f95846318c8bf314f6595
Kevin Canico <kevcanico25@gmail.com>
2021-06-17 14:03:48 +0200

    ✨ Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - competition-front',
-    title: 'competition-front',
+    titleTemplate: '%s - monkeys-front',
+    title: 'monkeys-front',
     htmlAttrs: {
       lang: 'en'
     },
