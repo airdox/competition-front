78ad6df855abfc392b091a040ffee8bdd140d1b2
Fabrice Cottin <fabrice.cottin1@viacesi.fr>
2021-06-23 11:31:33 +0200

    💄 mise en place du carousel

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,83 +1,109 @@
 <template>
-    <div class="hero">
-        <img
-            class="back"
-            alt="Flèche retour"
-            src="~/assets/images/back.svg"
-        >
+  <div class="hero">
+    <img class="back" alt="Flèche retour" src="~/assets/images/back.svg" />
 
-        <img
-            class="logo"
-            alt="Logo Persil et Romarin"
-            src="/logo.svg"
-        >
+    <img class="logo" alt="Logo Persil et Romarin" src="/logo.svg" />
 
-        <h1>Le parcours persillé</h1>
-        <hr>
-        <p class="paddingText">Accompagnez <span class="textPink">votre futur chef</span> tout au long de son parcours, du choix des recettes <span class="textPink"> jusqu’à votre assiette ! </span></p>
+    <h1>Le parcours persillé</h1>
+    <hr />
+    <p class="paddingText">
+      Accompagnez <span class="textPink">votre futur chef</span> tout au long de
+      son parcours, du choix des recettes
+      <span class="textPink"> jusqu’à votre assiette ! </span>
+    </p>
 
-        <p>Persil & Romain sont très curieux, il est très probable qu’ils vous posent quelques questions en chemin !</p>
+    <p>
+      Persil & Romain sont très curieux, il est très probable qu’ils vous posent
+      quelques questions en chemin !
+    </p>
 
-        <hr>
-        <p class="bold">Une surprise alléchante vous attend à l’arrivée...</p>
-        
-        <img
-            class="fondIllu"
-            alt="Icone"
-            src="~/assets/images/fondIcone.svg"
-        >
-        <button class="btn">Commencer</button>
-    </div>
+    <hr />
+    <p class="bold">Une surprise alléchante vous attend à l’arrivée...</p>
+
+    <img class="fondIllu" alt="Icone" src="~/assets/images/fondIcone.svg" />
+    <a href="#question1" style="text-decoration: none;">
+      <v-icon
+        style="font-size: 50px; cursor: pointer;"
+        large
+        color="white"
+        class="bounce"
+      >
+        mdi-arrow-down
+      </v-icon>
+    </a>
+  </div>
 </template>
 
 <style lang="scss" scope>
-    .container {
-        padding: 0 !important;
-        
+.container {
+  padding: 0 !important;
 
-        .hero {
-            padding-top: 50px;
-            background-color: #024A75;
-            padding-bottom: 10rem;
-            clip-path: polygon(0 0%, 100% 0%, 100% 100%, 0 94%);
+  .hero {
+    padding-top: 50px;
+    background-color: #024a75;
+    padding-bottom: 10rem;
+    clip-path: polygon(0 0%, 100% 0%, 100% 100%, 0 94%);
 
-            .back {
-                position: fixed;
-                left: 2.5rem;
-                top: 7rem;
-                cursor: pointer;
-            }
-        
-            .logo {
-                margin-bottom: 2.5rem;
-            }
-
-            h1 {
-                margin-bottom: 2.5rem;
-            }
-
-            h1, p {
-                padding: 0 6rem;
-            }
-
-            .paddingText {
-                margin-bottom: 4rem;
-            }
-
-            hr {
-                width: 1.6rem;
-                border: 1px solid $colorPink;
-                border-radius: 2rem;
-                margin: 2.5rem auto;
-            }
-
-            p.bold {
-                margin-bottom: 3rem;
-            }
-
-            .fondIllu {
-                width: 100%;
-            }
-        }
+    .bounce {
+      -moz-animation: bounce 2s infinite;
+      -webkit-animation: bounce 2s infinite;
+      animation: bounce 2s infinite;
     }
+
+    @keyframes bounce {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        transform: translateY(0);
+      }
+      40% {
+        transform: translateY(-30px);
+      }
+      60% {
+        transform: translateY(-15px);
+      }
+    }
+
+    .back {
+      position: fixed;
+      left: 2.5rem;
+      top: 7rem;
+      cursor: pointer;
+    }
+
+    .logo {
+      margin-bottom: 2.5rem;
+    }
+
+    h1 {
+      margin-bottom: 2.5rem;
+    }
+
+    h1,
+    p {
+      padding: 0 6rem;
+    }
+
+    .paddingText {
+      margin-bottom: 4rem;
+    }
+
+    hr {
+      width: 1.6rem;
+      border: 1px solid $colorPink;
+      border-radius: 2rem;
+      margin: 2.5rem auto;
+    }
+
+    p.bold {
+      margin-bottom: 3rem;
+    }
+
+    .fondIllu {
+      width: 100%;
+    }
+  }
+}
 </style>
