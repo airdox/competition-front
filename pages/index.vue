4e833b38a384514d600e21c1ed49279904fcd185
Fabrice Cottin <fabrice.cottin1@viacesi.fr>
2021-06-24 10:16:27 +0200

    🐛 fix bug ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="text-center">
+  <div class="notification">
+      <v-alert v-if="success" dismissible color="green" type="success"
+        >Votre bon a bien été envoyé !</v-alert
+      >
+      <v-alert v-if="error" dismissible color="red" type="error"
+        >Votre formulaire a mal été envoyé</v-alert
+      >
+    </div>
     <Hero />
-    <QuestionsCard />
+    <QuestionsCard @response="notification" />
   </div>
 </template>
 
@@ -14,7 +22,38 @@ export default {
     Hero,
     QuestionsCard
   },
+  data() {
+    return {
+      success: false,
+      error: false,
+    };
+  },
+  methods: {
+    notification(status) {
+      if (status === "success") {
+        this.success = true;
+      } else {
+        this.error = true;
+      }
+      setTimeout(function () {
+        this.success = false;
+        this.error = false;
+      }, 3000);
+    },
+  },
   
 };
 </script>
 
+
+<style lang="scss" scoped>
+.notification {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  top: 100px;
+  position: fixed;
+  z-index: 9999;
+}
+</style>
